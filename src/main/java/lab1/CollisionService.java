1315b4d6858d0baf8b4adcc674cd7b5479d0cc05
Johan <johan.svensson@svejo.se>
2026-02-26 11:36:18 +0100

    Collision

--- src/main/java/lab1/CollisionService.java
+++ src/main/java/lab1/CollisionService.java
@@ -1,4 +1,32 @@
 package lab1;
 
+
+//Hantera väggkollision här istället för i CarController
 public class CollisionService {
+    private final double minX, minY, maxX, maxY;
+
+    public CollisionService(double minX, double minY, double maxX, double maxY) {
+        this.minX = minX;
+        this.minY = minY;
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
+    public void handleWalls(Vehicle vehicle) {
+        double x = vehicle.getx();
+        double y = vehicle.gety();
+
+        boolean hitWall = x < minX || x > maxX || y < minY || y > maxY;
+        if (!hitWall) return;
+
+        // Flytta tillbaka inom rutan.
+        double clampedX = Math.max(minX, Math.min(x, maxX));
+        double clampedY = Math.max(minY, Math.min(y, maxY));
+
+        // Stanna + vänd + starta.
+        vehicle.stopEngine();
+        vehicle.setPosition(clampedX, clampedY);
+        vehicle.turnLeft();
+        vehicle.turnLeft();
+        vehicle.startEngine();
+    }
 }
--- src/main/java/lab1/WorkshopService.java
+++ src/main/java/lab1/WorkshopService.java
@@ -1,4 +1,10 @@
 package lab1;
 
 public class WorkshopService {
+    private final Workshop<Volvo240> workshop;
+
+    private final double workshopX;
+    private final double workshopY;
+    private final double loadRadius;
+    public WorkshopService(Works){}
 }
